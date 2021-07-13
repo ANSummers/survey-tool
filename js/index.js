a17ef8355782380724c3e088eb348de69b466785
Abbie <abbie@summers.gg>
2021-07-14 00:57:04 +0200

    removed unused js

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function currentSlide(n) {
 function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
+
   if (n > slides.length) {
     slidePosition = 1;
   }
@@ -28,9 +28,7 @@ function SlideShow(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace(" enable", "");
-  }
+
   slides[slidePosition - 1].style.display = "block";
   circles[slidePosition - 1].className += " enable";
 }
